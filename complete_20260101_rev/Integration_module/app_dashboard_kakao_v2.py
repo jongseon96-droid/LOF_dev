a3dbaf5f87cf5444282925a3b1c24494f01820a1
Jongsun Park <jongseon96@hanyang.ac.kr>
2026-01-05 20:11:39 +0900

    try

--- complete_20260101_rev/Integration_module/app_dashboard_kakao_v2.py
+++ complete_20260101_rev/Integration_module/app_dashboard_kakao_v2.py
@@ -190,6 +190,7 @@ def generate_kakao_html(center_lat, center_lon,
     <html>
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <style>html, body, #map {{ margin: 0; padding: 0; width: 100%; height: 100%; }}</style>
     </head>
     <body>
